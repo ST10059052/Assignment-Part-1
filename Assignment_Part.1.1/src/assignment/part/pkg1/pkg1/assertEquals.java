bdefc272aa6444e5b6254f0b6e2ae6f384270f57
SamuariX <jdv853786@gmail.com>
2022-04-11 15:31:26 +0200

    Assignment Part 1
    Due 19 April 2022

---
+++ Assignment_Part.1.1/src/assignment/part/pkg1/pkg1/assertEquals.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.part.pkg1.pkg1;
+
+
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+public class assertEquals {
+
+    private String username;
+    private String password;
+    private Object pass;
+    private boolean login;
+ 
+
+  
+    public void assertEquals (){
+      
+      
+       
+       System.out.println(login);
+     
+
+    
+        System.out.println(true);
+       
+  {
+        System.out.println(false);
+       
+    }
+         String rrdPattern = "^[a-zA-Z0-9_.][0 -1000](length = 5)$";
+        String[] rrdUserNames = {
+                
+                };
+        
+        
+            
+            if
+                    (!rrdUserName.matches){
+                System.out.println("kyl_1");
+                  System.out.println(true);
+                System.out.println("Username successfully captured");
+                
+            }else{
+                System.out.println("kyle!!!");
+             System.out.println(false);
+                System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length");
+                
+                
+            }
+         
+            String ddrPattern = "[A-Z][0 -1000][@,!,$,^,*,),+,:,,,>,|,#,~,%,&,(,-,/,.,<,?][length = 8]";
+       String[] ddrPasswords = {
+           
+       };
+      {
+          if
+                  (!ddrPassword.matches){
+      } else {
+              System.out.println("Ch&&sec@ke99!");
+              System.out.println(true);
+              System.out.println("Password successfully captured");
+              
+            }
+{    System.out.println("password");
+                     System.out.println(false);
+               System.out.println("Password correctly formatted, please ensure your username contains an underscore and is no more than 5 characters in length");
+           }
+      }
+      
+
+   
+    
+
+    
+}
+}
---
+++ Assignment_Part.1.1/src/assignment/part/pkg1/pkg1/pdrUserName.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.part.pkg1.pkg1;
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+class pdrUserName {
+
+    static boolean matches;
+
+    static boolean matches(String pdrPattern) {
+        
+        return (Boolean.FALSE);
+        
+    }
+
+    static void matches(String[] pdrUserNames) {
+        
+    }
+    
+}
