9cb3f4820f17e939ea4a4c664116fef2b5c20996
Jesse Viljoen(ST10059052) <102809419+ST10059052@users.noreply.github.com>
2022-04-13 12:28:02 +0200

    Add files via upload

---
+++ .github/workflows/assertEqualsTest.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package assignment.part.pkg1.pkg1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+public class assertEqualsTest {
+    
+    public assertEqualsTest() {
+    }
+    
+    /**
+     *
+     */
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of assertEquals method, of class assertEquals.
+     */
+    @Test
+    public void testAssertEquals() {
+        System.out.println("assertEquals");
+        assignment.part.pkg1.pkg1.assertEquals instance = new assignment.part.pkg1.pkg1.assertEquals();
+        instance.assertEquals();
+        // TODO review the generated test code and remove the default call to fail.
+        pass("The test case is a prototype.");
+    }
+
+    private void pass(String the_test_case_is_a_prototype) {
+        
+    }
+    
+}
---
+++ .github/workflows/sgdPassword.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.part.pkg1.pkg1;
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+class sgdPassword {
+
+    static boolean matches;
+    
+}
