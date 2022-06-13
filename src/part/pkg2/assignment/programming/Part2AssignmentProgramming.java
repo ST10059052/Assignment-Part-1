969c422f27e0457c0a3eca5a32058b65342a37f0
Jesse Viljoen(ST10059052) <102809419+ST10059052@users.noreply.github.com>
2022-06-13 14:49:53 +0200

    Add files via upload

---
+++ src/part/pkg2/assignment/programming/Part2AssignmentProgramming.java
@@ -0,0 +1,200 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package part.pkg2.assignment.programming;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+public class Part2AssignmentProgramming {
+
+    private static String strTaskNumber;
+    private static Object taskDescription;
+    private static String Task;
+    private static String strTaskName1;
+    private static String strTaskName;
+    private static long Duration;
+   
+    static String toUpperCase;
+   
+  
+
+    /**
+     * @param args the command line arguments
+ 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null,"Welcome to EasyKanban"); 
+        
+        String inputString, inputOption;
+         int option;
+   inputOption = JOptionPane.showInputDialog("Choose Option 1 - 3 if not program will fail");
+    option = Integer.parseInt(inputOption);
+    while (option == 1)
+        //After the user choose this option  display the add task option for the user to enter//
+    {
+        
+        inputOption = JOptionPane.showInputDialog("Add task ");
+        option = Integer.parseInt(inputOption);
+            
+       
+        for(int i =0; i<2;i++) {
+             String showInputDialog = JOptionPane.showInputDialog("How many task do you want? ");
+       
+           
+    }
+     
+                    
+}
+    String showInputDialog731 =JOptionPane.showInputDialog(null,"Task Name - Add login Feature(Must be mor than 4 characters ".toUpperCase()); 
+      
+      
+            
+            for (int i = 0; i<2; i++) {
+             strTaskNumber = JOptionPane.showInputDialog(null,"Task Number"); 
+            
+}
+       
+             String showInputDialog111 = JOptionPane.showInputDialog(null,"Enter Task Description......."+taskDescription); 
+         
+             if(!taskDescription(length()<50)){
+             JOptionPane.showMessageDialog(null,"Task successfully captured");
+             
+         } else {
+             
+             JOptionPane.showMessageDialog(null,"Please enter a task description of less than 50 characters");
+         }
+         String showInputDialog45 = JOptionPane.showInputDialog(null,"Developer Details - First and last name"); 
+        
+         long startTime = System.nanoTime();
+         methodToTime();
+         long endTime = System.nanoTime();
+         long duration = (endTime - startTime);
+      
+        
+          String strTaskName12=showInputDialog731.substring(0,1)+showInputDialog731.substring(1,2)+":"+ strTaskNumber + ":"+showInputDialog731.substring(showInputDialog731.length() - 3);
+       
+     
+                  
+Object[] options = {"To Do", "Done", "Doing"};
+        Object selectionObject = JOptionPane.showInputDialog(null, "Choose Task Status", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+      String selectionString = selectionObject.toString();
+        
+    Boolean:checkTaskDescription();
+String:createTaskID();
+String:printTaskDetails();
+Int: returnTotalHours();
+
+
+        Object[][] rows = {
+    {selectionString  ,showInputDialog45   ,strTaskNumber   ,showInputDialog731  ,showInputDialog111   ,strTaskName12  ,duration}};
+        Object[] cols = {
+    "Task Status"   ,"Developer Details"  ,"Task Number"  ,"Task Name"  , "Task Description" ,"Task ID" ,"Duration"
+        };
+JTable table = new JTable(rows, cols);
+JOptionPane.showMessageDialog(null, new JScrollPane(table));
+// This piece of code from Object to JOptionPane will display the information the user has typed into an table//
+
+
+while (option == 2)
+    {
+        
+        inputOption = JOptionPane.showInputDialog("Show report - this feature is still in development. Coming Soon (Choose Option 1 - 3 if not program will fail)");
+        option = Integer.parseInt(inputOption);
+    }
+  //This option is still in development//
+    while (option == 3)
+    {
+     
+        inputOption = JOptionPane.showInputDialog("Quit(Choose Option 1 - 3 if not program will fail)");
+        option = Integer.parseInt(inputOption);
+    
+      }
+        //This option lets the user quit the program//
+
+    
+} 
+  
+
+    private static int length() {
+        
+        return 50;
+        
+    }
+
+    private static boolean taskDescription(boolean b) {
+      
+        return true;
+      
+    }
+
+    private static void methodToTime() {
+     
+    }
+
+    private static void checkTaskDescription() {
+        
+         
+           
+    }
+
+    private static void createTaskID() {
+        
+  
+    
+          
+    }
+
+    private static void printTaskDetails() {
+       
+    }
+
+    private static void returnTotalHours() {
+       
+    }
+
+    private static class str {
+
+        private static String toUpperCase;
+
+        public str() {
+        }
+    }
+
+    private static class goTHer45 {
+
+        public goTHer45() {
+        }
+    }
+
+    private static class strArray {
+
+        private static String toString(int[] task3) {
+            
+            return null;
+            
+        }
+
+        public strArray() {
+        }
+    }
+} 
+         
+    
+    
+    
+    
+    
+ 
+    
+    
+    
+
---
+++ src/part/pkg2/assignment/programming/TestAssertEquals.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package part.pkg2.assignment.programming;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+
+public class TestAssertEquals {
+    private static Object taskDescription;
+      private static long Duration;
+    private int ij;
+      
+        
+        public void TestAsserEquals (){
+       if(!taskDescription(length()<50)){
+           Scanner keyboard = new Scanner(System.in);
+        java.lang.String name = keyboard.next();
+             System.out.println("Task successfully captured");
+            
+         } else {
+             
+            System.out.println("Please enter a task description of less than 50 characters");
+         }
+    String showInputDialog031 = "adbyn".toUpperCase();
+    String  strTaskNumber0 = "1";
+        String yiA3 =showInputDialog031.substring(0,1)+showInputDialog031.substring(1,2)+":"+ strTaskNumber0 + ":"+showInputDialog031.substring(showInputDialog031.length() - 3);
+                    System.out.println(yiA3);
+              int i = 0;
+             int u = 1;
+             int y = 2;
+             int r = 3;
+             
+      
+             while(ij < 1){
+                  String showInputDialog831 = "crike".toUpperCase();
+                  String showInputDialog839 = "crard".toUpperCase();
+                   String showInputDialog809 = "crtha".toUpperCase();
+                   String showInputDialog879 = "crnid".toUpperCase();
+                 
+                 System.out.println(showInputDialog831.substring(0,1)+showInputDialog831.substring(1,2)+":" + i +":"+showInputDialog831.substring(showInputDialog831.length() - 3));
+                   System.out.println(showInputDialog839.substring(0,1)+showInputDialog839.substring(1,2)+":" + u +":"+showInputDialog839.substring(showInputDialog839.length() - 3));
+                  System.out.println(showInputDialog809.substring(0,1)+showInputDialog809.substring(1,2)+":" + y +":"+showInputDialog809.substring(showInputDialog809.length() - 3));
+                  System.out.println(showInputDialog879.substring(0,1)+showInputDialog879.substring(1,2)+":" + r +":"+showInputDialog879.substring(showInputDialog879.length() - 3));
+                          ij++;
+             }
+              while(ij < 1){
+                  int task1 = 8;
+                  int task2 = 10;
+                  System.out.println(Duration +task1 + task2 +"hrs");
+                  ij++;
+              }
+              
+              int numTask1 = 10;
+               int numTask2 = 12;
+              int numTask3 = 55;
+                  int numTask4 = 11;
+              int numTask5 = 1;
+ System.out.println(Duration +numTask1 +numTask2 +numTask3+ numTask4+ numTask5 +"hrs");
+              
+}
+
+
+    private int length() {
+       return 0;
+    }
+
+    private boolean taskDescription(boolean b) {
+      
+        return true;
+      
+    }}
