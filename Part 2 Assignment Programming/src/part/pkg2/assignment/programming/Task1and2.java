e728d4927c7406b8ca0017b969b899ffddc3f7dd
Jesse Viljoen(ST10059052) <102809419+ST10059052@users.noreply.github.com>
2022-06-21 13:56:15 +0200

    Add files via upload

---
+++ Part 2 Assignment Programming/src/part/pkg2/assignment/programming/Task1and2.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package part.pkg2.assignment.programming;
+
+
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+
+public class Task1and2 {
+
+    private final int strTaskNumber = 3 ;
+   
+     
+    public void Task1 (){
+        
+System.out.println("login Feature");
+ for (int i = 0; i < 2; i++) {
+             System.out.println("Task Number"); 
+            
+}
+ 
+System.out.println("Create Login to authenticate users");
+System.out.println("Robyn Harrsion");
+System.out.println("8hrs");
+
+          String strTaskName12=showInputDialog731.substring(0,1)+showInputDialog731.substring(1,2)+":"+ strTaskNumber + ":"+showInputDialog731.substring(showInputDialog731.length() - 3);
+               System.out.println(strTaskName12);
+System.out.println("To Do");
+
+     }
+
+    /**
+     *
+     */
+     public void Task2 (){
+System.out.println("Add Task Feature");
+ for (int i = 0; i < 2; i++) {
+             System.out.println("Task Number"); 
+            
+}
+ 
+System.out.println("Create Add Task feature to add task users");
+System.out.println("Mike Smith");
+System.out.println("10hrs");
+String strTaskName12=showInputDialog731.substring(0,1)+showInputDialog731.substring(1,2)+":"+ strTaskNumber + ":"+showInputDialog731.substring(showInputDialog731.length() - 3);
+             System.out.println(strTaskName12);
+System.out.println("Doing");
+     
+    }
+
+    private static class showInputDialog731 {
+        
+
+        private static String substring(int i0,int i ) {
+         
+            
+            return "he";
+          
+        }
+
+        private static int length() {
+          
+            return 20;
+          
+        }
+
+        private static String substring(int i) {
+        
+            return "llo";
+        
+        }
+
+        public showInputDialog731() {
+            System.out.println("Hello");
+        }
+    }
+}
+    
---
+++ Part 2 Assignment Programming/src/part/pkg2/assignment/programming/string.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package part.pkg2.assignment.programming;
+
+/**
+ *
+ * @author Jesse Viljoen
+ */
+class string {
+
+    static int length() {
+     
+        return 0;
+     
+    }
+
+    static int substring(int i) {
+        
+        return 0;
+        
+    }
+    
+}
